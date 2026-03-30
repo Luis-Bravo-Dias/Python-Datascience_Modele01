b9c096e71d621fb4d1a980f0d91a8d082057bf5f
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s6.42lisboa.com>
2026-03-30 17:39:19 +0100

    2

--- ex00/give_bmi.py
+++ ex00/give_bmi.py
@@ -5,12 +5,13 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
  bmi = []
  for h, w in zip(height, weight):
    if not isinstance(h, (int, float)) or not isinstance(w, (int, float)):
-     raise TypeError("Invalid value type")
+     raise TypeError("Invalid value type, use int or float")
    if h <= 0 or w <= 0:
     raise ValueError("Height and Weight cannot be zero or negative")
    bmi.append(w / (h * h))
  return bmi
 
-#def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-#your code here
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+ if not isinstance(limit, int):
+  raise TypeError("Invalid value type, use int or float")
 
