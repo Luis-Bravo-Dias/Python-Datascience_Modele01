a508aa058707501e8f9380f6935b33b8ed48579b
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r1s6.42lisboa.com>
2026-04-17 12:41:55 +0100

    ex01

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -2,6 +2,17 @@ import numpy as np
 
 def slice_me(family: list, start: int, end: int) -> list:
  
+ if not isinstance(family, list):
+  raise TypeError("Invalid value type, use lists")
+ if len(family) == 0:
+  raise TypeError("The list shouldn't be empty")
+ for row in family:
+    if not isinstance(row, list):
+        raise TypeError("It should be a list of lists")
+ first_len = len(family[0])
+ for row in family:
+  if len(row) != first_len:
+   raise TypeError("Invalid value type, use int or float")
  fam = np.array(family)
  rows_1, cols_1 = fam.shape
  print(f"My shape is : ({rows_1}, {cols_1})")
